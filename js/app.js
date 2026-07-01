7709a542eb9149ce74430b58670c3f4884b44f2b
gayathrikaushalya-tech <kaushalyagayathri1993@gmail.com>
2026-07-01 08:50:55 -0700

    6th commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
   console.log("Hello Java Script :)");
+  let name = "OKITH";
+  console.log(name);
+  let response =true;
+  console.log(response);
+  name = "THINUJA";
+   console.log(name);
+alert("Hello Okith")
+console.log(typeof name);
+console.log(typeof response);
+let age =10;
+let price = 12.50;
+let customers = [];
+let box ={};
+let isAfterAL = false;
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof customers);
+console.log(typeof box);
+console.log(typeof isAfterAL);
+let x = 10;
+let y = 20;
+let z = x + y ;
+console.log(z);
+let b = y/x;
+console.log(b);
+let x1 = 10;
+let y1 = 10;
+console.log(x1==y1);
+
+let x2= 10;
+let y2 = "10";
+console.log(x2==y2);
+if(true && true){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(true && false){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(true ||true){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(true || false){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(false || false){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(!false ){
+  console.log("true");
+}else{
+ console.log("false");
+}
+if(!true){
+  console.log("true");
+}else{
+ console.log("false");
+}
